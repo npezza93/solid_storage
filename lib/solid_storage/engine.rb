3e3d3354eff89692a45a033906af85f9351612b3
Nick Pezza <pezza@hey.com>
2024-10-18 15:48:47 -0400

    Setup config

--- lib/solid_storage/engine.rb
+++ lib/solid_storage/engine.rb
@@ -1,5 +1,13 @@
 module SolidStorage
   class Engine < ::Rails::Engine
     isolate_namespace SolidStorage
+
+    config.solid_storage = ActiveSupport::OrderedOptions.new
+
+    initializer "solid_storage.config" do
+      config.solid_storage.each do |name, value|
+        SolidStorage.public_send(:"#{name}=", value)
+      end
+    end
   end
 end
